7b41e8f9ce1d95b8b3973c162b1b48b1ed1d009b
Paul Negoescu <paul.negoescu@nagarro.com>
2024-10-08 10:49:36 +0300

    Clean slate

--- dist/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-console.log('Hello from JavaScript 2 automatizat!');
-let a = 12;
-a = 'Paul';
--- index.ts
+++
@@ -1,4 +0,0 @@
-console.log('Hello from JavaScript 2 automatizat!');
-
-let a = 12;
-a = 'Paul';
