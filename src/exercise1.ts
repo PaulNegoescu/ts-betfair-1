c1943443be8482bcb6ec443ea87801bb2b0b2481
Paul Negoescu <paul.negoescu@nagarro.com>
2024-10-15 10:50:46 +0300

    Implementation of RPS and types

--- src/exercise1.ts
+++ src/exercise1.ts
@@ -3,8 +3,17 @@
 // Fiecare alege aleatoriu o optiune
 // In consola se afiseaza rezultatul jocului
 
-interface Options {
+const choices = ['rock', 'paper', 'scissors'] as const;
 
+type Choices = (typeof choices)[number];
+
+interface OptionConfig {
+  beats: [Choices];
+  flavorText: [string];
+}
+
+type Options = {
+  [key in Choices]: OptionConfig;
 }
 
 const options: Options = {
@@ -22,11 +31,29 @@ const options: Options = {
   }
 };
 
+// const choices = Object.keys(options) as [Choices];
+
 export default function play() {
   let outcomeText = '';
+
+  const player1Choice = choices[getRandomIndex()];
+  const player2Choice = choices[getRandomIndex()];
+
+  if(player1Choice === player2Choice) {
+    outcomeText = `It's a draw! Both selected: ${player1Choice}`;
+  } else if (options[player1Choice].beats.includes(player2Choice)) {
+    outcomeText = `Player 1 wins! ${capitalize(player1Choice)} ${options[player1Choice].flavorText[0]} ${player2Choice}`;
+  } else {
+    outcomeText = `Player 2 wins! ${capitalize(player2Choice)} ${options[player2Choice].flavorText[0]} ${player1Choice}`;
+  }
+
   return outcomeText;
 }
 
 function getRandomIndex(max = 2) {
   return Math.floor(Math.random() * (max + 1));
+}
+
+function capitalize(str: string) {
+  return str[0].toUpperCase() + str.substring(1);
 }
